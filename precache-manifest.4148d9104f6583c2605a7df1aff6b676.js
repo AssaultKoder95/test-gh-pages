eec10995033e3e24c1d5a1329ff94e977b9ead18
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2021-05-20 14:23:58 +0530

    Updates

---
+++ precache-manifest.4148d9104f6583c2605a7df1aff6b676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69639aafaf4da51ca236ed7b96bd92b8",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "190939d5261ad8c40ddc",
+    "url": "/test-gh-pages/static/css/main.d8f5e53f.chunk.css"
+  },
+  {
+    "revision": "fb37ec207222f6209bc5",
+    "url": "/test-gh-pages/static/js/2.154cceaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-gh-pages/static/js/2.154cceaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190939d5261ad8c40ddc",
+    "url": "/test-gh-pages/static/js/main.68050a13.chunk.js"
+  },
+  {
+    "revision": "d180cbfe623ff15c4937",
+    "url": "/test-gh-pages/static/js/runtime-main.2e4615fa.js"
+  }
+]);
